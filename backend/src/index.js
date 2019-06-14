87395d41c3884f8102cec1d7212e094a7c34fb8c
Erislândio Soares <erislandio.soares@acct.global>
2019-06-13 22:22:53 -0300

    add type file name jpg to save data

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,15 @@
 const mongoose = require("./database/index");
 const routes = require("./routes");
 const express = require("express");
+const path = require("path");
 const app = express();
 const chalk = require("chalk");
 const PORT = 3333;
 
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "uploads", "resized"))
+);
 app.use(routes);
 
 app.listen(PORT, () => {
