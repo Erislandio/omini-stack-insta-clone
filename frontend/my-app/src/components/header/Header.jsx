72123963ccbf076563207ada85a3369b5c49a7ad
Erislândio Soares <erislandio.soares@acct.global>
2019-06-16 00:46:10 -0300

    add roue header and template

---
+++ frontend/my-app/src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './header.css'
+
+export default function Header() {
+    return (
+        <header id="main-header">
+            <div className="header-content">
+                <img src="" alt="logo" />
+                <img src="" title="Enviar publicação" alt="Enviar publicação" />
+            </div>
+        </header>
+    )
+}
